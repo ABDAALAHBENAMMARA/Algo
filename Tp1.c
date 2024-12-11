0d2b82bb53d87bc39998e98f482172519e5d4a7e
ABDAALAHBENAMMARA <benammaraabdalah110@gmail.com>
2024-12-11 12:15:47 +0100

    Update Tp1.c

--- Tp1.c
+++ Tp1.c
@@ -1,1 +1,39 @@
-Kfjfjfkckkcg
+#include<stdio.h> 
+#include<stdlib.h> 
+int main()
+{
+int n= 4,i,j,k;
+
+int A [4][4]={{1, 2, 3, 4},{5, 6, 7, 8},{9, 10, 11, 12},{13, 14, 15, 16}}; 
+
+printf("Original matrix:\n");
+
+for (i = 0 ; i<n; i++) {
+ 
+ for ( j = 0 ;j<n; j++) {
+
+ printf("%d\t ", A [i][j]); 
+} 
+printf("\n");
+} 
+
+
+for(i = 1 ; i<n; i++) {
+
+for (j=0; j<i+1; j++) { 
+k=A[i][j];
+A[i][j]= A[j][i];
+A[j][i] =k;
+}
+} 
+
+printf ("afrte chainge: \n");
+
+for (i = 0 ; i<n; i++) {
+for ( j = 0 ;j<n; j++) {
+printf("%d\t", A[i][j]);
+} 
+printf("\n");
+} 
+return 0;
+} 
